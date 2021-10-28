9d3e475f113d6d803b25722999bc5dd49579f3a4
Francisco Justo <franciscojusto.dev@gmail.com>
2021-10-28 16:14:52 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,88 @@
+import pygame
+import random
+import os
+
+WIDTH = 800
+HEIGHT = 400
+FPS = 30
+STD_SIZE = (50, 50)
+
+pygame.init()
+pygame.mixer.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("FlappyBiwwwaaaard!")
+clock = pygame.time.Clock()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.direction = pygame.math.Vector2(0, 0)
+        self.player_speed = 2.2
+        self.gravity = 0.8
+
+    def player_movement(self):
+        # initial movement
+        self.direction.y += self.gravity
+        self.direction.x = self.player_speed
+        if self.rect.left > WIDTH - (WIDTH / 2.5):
+            self.player_speed = 0
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            self.direction.y = -10
+
+    def update(self):
+        self.player_movement()
+        self.rect.x += self.direction.x * self.player_speed
+        self.rect.y += self.direction.y * self.gravity
+
+
+class Level(pygame.sprite.Sprite):
+    def __init__(self):
+        self.last = pygame.time.get_ticks()
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface(
+            (50, random.randrange(100, HEIGHT - (HEIGHT / 2))))
+        self.image.fill((0, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.top = 0
+        self.rect.x = random.randrange(WIDTH, WIDTH + (WIDTH / 4))
+        self.flag = True
+
+    def spawn(self):
+        # if enough time passed, spawn another wall
+        now = pygame.time.get_ticks()
+        if now - self.last >= 3000:
+            self.last = now
+            level = Level()
+            all_sprites.add(level)
+
+    def update(self):
+        if player.player_speed <= 0:
+            self.rect.x -= 2.2
+        if self.flag is True:
+            self.spawn()
+            self.flag = False
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+level = Level()
+all_sprites.add(player)
+all_sprites.add(level)
+
+while True:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    all_sprites.update()
+
+    screen.fill((0, 0, 0))
+    all_sprites.draw(screen)
+    pygame.display.flip()
