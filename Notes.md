630118639416025d491ae8d5be02db82dd9856cb
Francisco Justo <franciscojusto.dev@gmail.com>
2021-10-30 22:58:25 +0100

    added obstacles to top and bottom of screen

--- Notes.md
+++ Notes.md
@@ -1,7 +1,14 @@
-##FlappyBird
+
+# FlappyBird
 
 This is a game about a flying bird or weird creature!
 
-#To Do:
-fix jump wait time
-make bottom obstacles appear!
+## To Do:
+
+add collision detection between player and obstacles  
+adjust gravity, jump force and pbstcle speed values!
+
+### ADDED:
+obstacles now appear in both ends of screen!  
+delay between jumps  
+separate groups for player and obstacles  
--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import os
 WIDTH = 800
 HEIGHT = 400
 FPS = 30
-STD_SIZE = (50, 50)
+STD_SIZE = (64, 64)
+JUMP_FORCE = -10
+JUMP_DELAY = 3
+OBSTACLE_GAP = STD_SIZE[0] + 100
+OBSTACLE_SPEED = 2.2
 
 pygame.init()
 pygame.mixer.init()
@@ -18,7 +22,7 @@ clock = pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
+        self.image = pygame.Surface(STD_SIZE)
         self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
@@ -28,74 +32,80 @@ class Player(pygame.sprite.Sprite):
 
     def player_movement(self):
         # initial movement
-        self.direction.y += self.gravity
         self.direction.x = self.player_speed
         if self.rect.left > WIDTH - (WIDTH / 2.5):
             self.player_speed = 0
+        
+    def jump(self):
         if pygame.key.get_pressed()[pygame.K_SPACE]:
-            self.direction.y = -10
-
+            self.direction.y = JUMP_FORCE
+        
     def update(self):
+        self.direction.y += self.gravity
         self.player_movement()
+        if self.direction.y >= JUMP_DELAY:
+            self.jump()
         self.rect.x += self.direction.x * self.player_speed
         self.rect.y += self.direction.y * self.gravity
 
-
-class Level(pygame.sprite.Sprite):
+class Level_Manager(object):
     def __init__(self):
-        self.last = pygame.time.get_ticks()
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface(
-            (50, random.randrange(100, int(HEIGHT - (HEIGHT / 3)))))
-        self.image.fill((0, 255, 255))
-        self.rect = self.image.get_rect()
-        self.rect.top = 0
-        self.x = random.randrange(WIDTH, int(WIDTH + (WIDTH / 4)))
-        self.rect.x = self.x
-        self.image_dwn = pygame.Surface((50, HEIGHT))
-        self.image_dwn.fill((255, 0, 0))
-        self.rect_dwn = self.image_dwn.get_rect()
-        self.rect_dwn.top = random.randrange(
-            int(HEIGHT / 2), int(HEIGHT - (HEIGHT / 8)))
-        self.rect_dwn.x = self.x
         self.flag = True
 
-    def spawn(self):
-        # if enough time passed, spawn another wall
+    def update(self):
         now = pygame.time.get_ticks()
+        self.y_size = random.randrange(100, int(HEIGHT - (HEIGHT / 3)))
+        if self.flag is True:
+            obstacle1 = Obstacle(self.y_size, 0)
+            obstacle_sprites.add(obstacle1)
+            all_sprites.add(obstacle1)
+            obstacle2 = Obstacle(HEIGHT, self.y_size + OBSTACLE_GAP)
+            obstacle_sprites.add(obstacle2)
+            all_sprites.add(obstacle2)
+            self.last = pygame.time.get_ticks()
+            self.flag = False
         if now - self.last >= 3000:
             self.last = now
-            level = Level()
-            all_sprites.add(level)
-            return False
-        else:
-            return True
+            self.flag = True
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, size, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.last = pygame.time.get_ticks()
+        self.image = pygame.Surface(
+            (STD_SIZE[0], size))
+        self.image.fill((0, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.top = y
+        self.rect.x = WIDTH
 
     def update(self):
         if player.player_speed <= 0:
-            self.rect.x -= 2.2
-            self.rect_dwn.x -= 2.2
-        if self.flag is True:
-            self.flag = self.spawn()
+            self.rect.x -= OBSTACLE_SPEED
         if self.rect.right < 0:
             self.kill()
-            print("super dead sprite!")
-
+            print("super dead sprite -top!")   
 
 all_sprites = pygame.sprite.Group()
+player_sprite = pygame.sprite.Group()
+obstacle_sprites = pygame.sprite.Group()
+manager = Level_Manager()
 player = Player()
-level = Level()
+player_sprite.add(player)
 all_sprites.add(player)
-all_sprites.add(level)
 
 while True:
     clock.tick(FPS)
+    # input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
+    # update
+    manager.update()
     all_sprites.update()
 
+    #draw
     screen.fill((0, 0, 0))
     all_sprites.draw(screen)
     pygame.display.flip()
